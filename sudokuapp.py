825283e13ef4ba601c82ba52839e74bf93188fca
mattpero22 <68607936+mattpero22@users.noreply.github.com>
2020-08-13 09:41:42 -0400

    Update sudokuapp.py

--- sudokuapp.py
+++ sudokuapp.py
@@ -1,19 +1,16 @@
-import numpy
-
-possible_numbers=[1,2,3,4,5,6,7,8,9]
-hash=[0,0,0,0,0,0,0,0,0]
 #######################################################################
 #sample board
-board= [[0,0,0,2,6,0,7,0,1],
-        [6,8,0,0,7,0,0,9,0],
-        [1,9,0,0,0,4,5,0,0],
-        [8,2,0,1,0,0,0,4,0],
-        [0,0,5,6,0,2,9,0,0],
-        [0,5,0,0,0,3,0,2,8],
-        [0,0,9,3,0,0,0,7,4],
-        [0,4,0,0,5,0,0,3,6],
-        [7,0,3,0,1,3,0,0,0]]
-########################################################################
+board= [[0,5,0,0,0,4,0,1,0],
+        [0,0,3,0,0,1,0,0,9],
+        [0,0,4,3,0,0,5,7,0],
+        [2,0,0,8,9,0,0,5,0],
+        [6,0,5,4,1,2,9,0,7],
+        [0,7,0,0,5,3,0,0,2],
+        [0,9,6,0,0,5,2,0,0],
+        [5,0,0,2,0,0,8,0,0],
+        [0,2,0,1,0,0,0,9,0]]
+val=[1,2,3,4,5,6,7,8,9]
+#########################################################################
 #display in console
 def display_unsolved():
     for i in range(9):
@@ -25,53 +22,117 @@ def display_unsolved():
                 print("|",end="")
             if(j==8):
                 print("\n",end="")
-#########################################################################
+##########################################################################
+#macroscopic location of cell for checking 3x3
+def checkzone(val,i,j):
+    if(i>=0 and i<=2 and j>=0 and j<=2): #NW cell
+        x=0
+        y=0
+        for x in range(3):
+            for y in range(3):
+                if(board[x][y]!=0 and val.count(board[x][y])!=0):
+                    val.remove(board[x][y])
+
+    if(i>=0 and i<=2 and j>=3 and j<=5): #N cell
+        x=0
+        y=0
+        for x in range(3):
+            for y in range(3):
+                if(board[x][y+3]!=0 and val.count(board[x][y+3])!=0):
+                    val.remove(board[x][y+3])
+
+    if(i>=0 and i<=2 and j>=6 and j<=8): #NE cell
+        x=0
+        y=0
+        for x in range(3):
+            for y in range(3):
+                if(board[x][y+6]!=0 and val.count(board[x][y+6])!=0):
+                    val.remove(board[x][y+6])
+
+    if(i>=3 and i<=5 and j>=0 and j<=2): #W cell
+        x=0
+        y=0
+        for x in range(3):
+            for y in range(3):
+                if(board[x+3][y]!=0 and val.count(board[x+3][y])!=0):
+                    val.remove(board[x+3][y])
+
+    if(i>=3 and i<=5 and j>=3 and j<=5): #C cell
+        x=0
+        y=0
+        for x in range(3):
+            for y in range(3):
+                if(board[x+3][y+3]!=0 and val.count(board[x+3][y+3])!=0):
+                    val.remove(board[x+3][y+3])
+
+
+    if(i>=3 and i<=5 and j>=6 and j<=8): #E cell
+        x=0
+        y=0
+        for x in range(3):
+            for y in range(3):
+                if(board[x+3][y+6]!=0 and val.count(board[x+3][y+6])!=0):
+                    val.remove(board[x+3][y+6])
+
+    if(i>=6 and i<=8 and j>=0 and j<=2): #SW cell
+        x=0
+        y=0
+        for x in range(3):
+            for y in range(3):
+                if(board[x+6][y]!=0 and val.count(board[x+6][y])!=0):
+                    val.remove(board[x+6][y])
+
+    if(i>=6 and i<=8 and j>=3 and j<=5): #S cell
+        x=0
+        y=0
+        for x in range(3):
+            for y in range(3):
+                if(board[x+6][y+3]!=0 and val.count(board[x+6][y+3])!=0):
+                    val.remove(board[x+6][y+3])
+
+    if(i>=6 and i<=8 and j>=6 and j<=8): #SE cell
+        x=0
+        y=0
+        for x in range(3):
+            for y in range(3):
+                if(board[x+6][y+6]!=0 and val.count(board[x+6][y+6])!=0):
+                    val.remove(board[x+6][y+6])
+########################################################################
 #check row digits
-def checkrow(i):
-    global hash
+def checkrow(val,i):
     for x in range(9):
-        if(board[i][x]!=0):
-            hash[(board[i][x])-1]=(board[i][x])
+        if(board[i][x]!=0 and val.count(board[i][x])!=0):
+            val.remove(board[i][x])
 #########################################################################
 #check column digits
-def checkcol(j):
-    global hash
+def checkcol(val,j):
     for x in range(9):
-        if(board[x][j]!=0):
-            hash[(board[x][j])-1]=(board[x][j])
+        if(board[x][j]!=0 and val.count(board[x][j])!=0):
+            val.remove(board[x][j])
 #########################################################################
-#check 3x3 zone digits(only the 4 remaining cells)
-def checkzone(i,j):
-    loc=""
-    global hash
-    if(i==0 or i==3 or i==6):
-        loc=loc+"N"
-    if(i==2 or i==5 or i==8):
-        loc=loc+"S"
-    if(j==0 or j==3 or j==6):
-        loc=loc+"W"
-    if(j==2 or j==5 or j==8):
-        loc=loc+"E"
-
-    print(loc)
-
-    return
+def checkall(val,i,j):
+    checkrow(val,i)
+    checkcol(val,j)
+    checkzone(val,i,j)
+    return val
 #########################################################################
 #function to solve puzzle
-def solver():
-    global hash
+def solver(val):
+    global board
+    n=0
     for i in range(9):
         for j in range(9):
             if(board[i][j]==0):
-                checkrow(i)
-                checkcol(j)
-                checkzone(i,j)
-                print(hash)
-                hash=[0]*9      #reset hash table of valid digits
+                x=checkall(val,i,j)
+                if(len(x)>0):
+                    board[i][j]=x[n]
+                else
 
+##########################################################################
 def main():
     display_unsolved()
-    solver()
-    return
+    solver(val)
+    print("\n")
+    display_unsolved()
 
 main()
